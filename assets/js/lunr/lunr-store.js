121a8bcb4777e9c1d2f8b4ac39db8dfe59409516
Flo M <flo@local>
2019-12-25 04:33:04 +0200

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Welcome",
         "excerpt":"Hi and like the title suggest, welcome to my personal website, blog, voice on the Internet, etc.  ","categories": ["First step"],
         "tags": ["welcome","bienvenue","bineativenit","willkommen"],
-        "url": "https://www.florianmadar.com/https://www.florianmadar.com/first%20step/first-post/",
+        "url": "https://www.florianmadar.com/first%20step/first-post/",
         "teaser":null}]
