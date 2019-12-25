3052d4bdd5078a88c7cb628fcf540c162c7aca30
Flo M <flo@local>
2019-12-25 04:03:25 +0200

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Welcome",
         "excerpt":"Hi and like the title suggest, welcome to my personal website, blog, voice on the Internet, etc.  ","categories": ["First step"],
         "tags": ["welcome","bienvenue","bineativenit","willkommen"],
-        "url": "/first%20step/first-post/",
+        "url": "http://localhost:4000/first%20step/first-post/",
         "teaser":null}]
